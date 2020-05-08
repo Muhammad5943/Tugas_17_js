98b2f32418c39cda6eefd040970caa8ad8aa5059
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-05-08 21:41:16 +0700

    tugas17js
    mengumpulkan tugas 17 js

---
+++ tugas17.js
@@ -0,0 +1,29 @@
+function tugas17() {
+    var huruf1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+    console.log(huruf1.toLocaleLowerCase());
+    console.log(huruf1.toLocaleUpperCase());
+
+    console.log("");
+    
+
+    var huruf2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+    console.log(huruf2.toLocaleLowerCase());
+    console.log(huruf2.toLocaleUpperCase());    
+    
+}
+
+tugas17()
+
+console.log("");
+
+
+function tugas172() {
+    var x = "Lorem Ipsum";
+
+    console.log("L".charCodeAt() + "-" + "o".charCodeAt() + "-" + "r".charCodeAt() + "-" + "e".charCodeAt() + "-" + 
+    "m".charCodeAt() + "-" + " ".charCodeAt());
+     
+     
+}
+
+tugas172()
